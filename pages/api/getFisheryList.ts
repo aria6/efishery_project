79c2ec1f5d7a2a8c85c09011f6089ebfaff7cbca
aria6 <dian.ariawal@gmail.com>
2021-12-06 10:40:10 +0700

    feat(enhancement): Add filter list on getFisheryList API

--- pages/api/getFisheryList.ts
+++ pages/api/getFisheryList.ts
@@ -1,12 +1,25 @@
 import getList from '../../src/hooks/useGetList';
 import convertCurrency from '../../src/hooks/useGetValueCurrency';
 
-export default async function getFisheryList(_: any, res: any) {
+export default async function getFisheryList(req: any, res: any) {
   let response = await getList();
   let currencyValue = await convertCurrency({ from: 'IDR', to: 'USD' });
 
+  let province = req.query.province;
+  let city = req.query.city;
+  let commodity = req.query.commodity;
+  let size = req.query.size;
+
   let listFishery = response
     .filter((item: FisheryItem) => item.uuid)
+    .filter((item: FisheryItem) =>
+      province ? province === item.area_provinsi : true
+    )
+    .filter((item: FisheryItem) => (city ? city === item.area_kota : true))
+    .filter((item: FisheryItem) =>
+      commodity ? commodity === item.komoditas : true
+    )
+    .filter((item: FisheryItem) => (size ? size === item.size : true))
     .map((item: FisheryItem) => ({
       ...item,
       usd_price: currencyValue ? Number(item.price) * currencyValue : 0,
