368e60b7426b44f668b4a2cdbb735e467dce5eec
aria6 <dian.ariawal@gmail.com>
2021-12-06 11:13:41 +0700

    feat(config): Add caching mechanisme on all path

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=10, stale-while-revalidate=59',
+            
+          }
+        ],
+      },
+    ]
+  },
 }
