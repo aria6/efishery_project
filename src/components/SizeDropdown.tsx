0648afa5de68f814acc98ae4e0933f017b76e830
aria6 <dian.ariawal@gmail.com>
2021-12-06 10:14:42 +0700

    feat(init): Add Size dropdown component

---
+++ src/components/SizeDropdown.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import useSWR from 'swr';
+
+import { Dropdown } from '../core-ui';
+import { fetchAPI } from '../helpers';
+
+type DropdownProps = {
+  value: string;
+  onChange: (value: string) => void;
+};
+
+export default function SizeDropdown({ onChange, value }: DropdownProps) {
+  let { data } = useSWR(`/api/getSize`, fetchAPI);
+  return (
+    <Dropdown
+      label='Size :'
+      options={data?.data ?? []}
+      onChange={onChange}
+      value={value}
+    />
+  );
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -4,4 +4,5 @@ export {default as InformationBox} from './InformationBox';
 export {default as ProvinciesDropdown} from './ProvinciesDropdown';
 export {default as CitiesDropdown} from './CitiesDropdown';
 export {default as CommoditiesDropdown} from './CommoditiesDropdown';
+export {default as SizeDropdown} from './SizeDropdown';
 export {default as Loading} from './Loading';
